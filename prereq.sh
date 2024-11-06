da1b8fcb9750fff91f9ca2e298c5cc09553684e4
Youngchae Chee <litcoderr@gmail.com>
2024-11-06 08:49:11 +0000

    impl: start ixc2b5

--- prereq.sh
+++ prereq.sh
@@ -7,6 +7,6 @@ export LD_LIBRARY_PATH="/usr/lib64-nvidia"
 #cp /var/nvidia-driver-local-repo-ubuntu2204-565.57.01/nvidia-driver-local-A03A1BE8-keyring.gpg /usr/share/keyrings/
 #dpkg -i nvidia-driver-local-repo-ubuntu2204-565.57.01_1.0-1_amd64.deb 
 
-
 mkdir ~/dataset
 pip install datasets
+pip install decord
