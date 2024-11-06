350b3435d393987d6c0806043d94489ae012ff19
Youngchae Chee <litcoderr@gmail.com>
2024-11-06 09:37:29 +0000

    update

--- prereq.sh
+++ prereq.sh
@@ -5,14 +5,15 @@ git config --global user.name "Youngchae Chee"
 
 export LD_LIBRARY_PATH="/usr/lib64-nvidia"
 
-ANACONDA_VERSION="Anaconda3-2023.07-1-Linux-x86_64.sh"
-curl -O https://repo.anaconda.com/archive/$ANACONDA_VERSION
-bash $ANACONDA_VERSION -b -p $HOME/anaconda3
-export PATH="$HOME/anaconda3/bin:$PATH"
-echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> ~/.bashrc
-source $HOME/anaconda3/bin/activate
-conda init --all --dry-run --force
-rm $ANACONDA_VERSION
+# ANACONDA_VERSION="Anaconda3-2023.07-1-Linux-x86_64.sh"
+# curl -O https://repo.anaconda.com/archive/$ANACONDA_VERSION
+# bash $ANACONDA_VERSION -b -p $HOME/anaconda3
+# export PATH="$HOME/anaconda3/bin:$PATH"
+# echo 'export PATH="$HOME/anaconda3/bin:$PATH"' >> ~/.bashrc
+# source $HOME/anaconda3/bin/activate
+# conda init --all --dry-run --force
+# rm $ANACONDA_VERSION
 
+pip install flash-attn --no-build-isolation
 pip install datasets
 pip install decord
